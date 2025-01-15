f9d625fe6604bbb62287ba283a81a11c9ff7b162
Etolzl <149280430+Etolzl@users.noreply.github.com>
2025-01-15 11:58:16 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+*/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/github-pages/', // Cambia esto al nombre de tu repositorio en GitHub
 })
